1a4d905e20a4f1b685d8b561f1a4ba6a5d7e289f
yangjijun <yangjijun@westlake.edu.cn>
2024-12-12 13:25:40 +0800

    updated

--- config_pulse_gen.py
+++ config_pulse_gen.py
@@ -1,58 +1,60 @@
 import pyvisa
 import sys
-pulse_generator = 'USB0::0xF4EC::0x1101::SDG6XFCD7R0976::INSTR'
+# pulse_generator = 'USB0::0xF4EC::0x1101::SDG6XFCD7R0976::INSTR'
+pulse_generator = 'TCPIP0::192.168.4.9::INSTR'
 def configure_waveform_generator(     
     fre: int = 50, 
-    amp: float = 1.8,   
-    delay: int = 5
+    amp: float = 1.8,     
+    delay: int = 5,
+    sync: str = 'CH2',
+    comb = True
     ):
     rm = pyvisa.ResourceManager()
     afg = rm.open_resource(pulse_generator)
-    
-    SYNC_CH = 'CH2'
-    afg.write('C2:SYNC ON, TYPE,{}'.format(SYNC_CH))
-    print(r'external trigger Sync with {}'.format(SYNC_CH))
-    # if delay <= 5:
-    #     print(r'external trigger Sync with S1, delay time:{}'.format(delay))
-    #     SYNC_CH = 'CH1'
-    #     afg.write('C1:SYNC ON, TYPE,{}'.format(SYNC_CH))
-    # elif delay > 5:
-    #     print(r'delay time {} us large than 5us, external trigger Sync with S2'.format(delay))
-    #     SYNC_CH = 'CH2'
-    # afg.write('C2:SYNC ON, TYPE,{}'.format(SYNC_CH))
+    if sync == 'CH1':
+        SYNC_CH = sync 
+        afg.write('C1:SYNC ON, TYPE,{}'.format(SYNC_CH))
+        amp_2 = 0
+        ch2_on = 'OFF'
+    elif sync == 'CH2':
+        SYNC_CH = sync
+        afg.write('C2:SYNC ON, TYPE,{}'.format(SYNC_CH))
+        amp_2 = 1.36 
+        ch2_on = 'ON'
 
-    # print(r'SYNC_CH:{}'.format(SYNC_CH))
-    # if amp != offset+offset:
-    #     print(r'amp:{} != 2*offset:{}'.format(amp,2*offset))
-    #     print('Warning: Amplitude and offset are not correctly, please check!')
-    #     sys.exit()
         
-    afg.write('C1:CoMBiNe ON')
+   
+    if comb == True:
+        afg.write('C1:CoMBiNe ON')       # CH1组合输出配置为ON  
+    elif comb == False:
+        afg.write('C1:CoMBiNe OFF')       # CH1组合输出配置为ON  
+    
+
 
     # 配置输出通道的命令
-    commands = [  
+    commands = [
         'C1:OUTP ON',
         'C1:OUTP LOAD,50',
         'C1:OUTP PLRT,NOR',
         'C1:BSWV WVTP,PULSE',  
-        'C2:OUTP ON',
+        r'C2:OUTP {}'.format(ch2_on),
         'C2:OUTP LOAD,50',
         'C2:OUTP PLRT,NOR',
         'C2:BSWV WVTP,PULSE', 
-        'C1:BSWV FRQ,{}'.format(fre),      # 频率配置为50 Hz
-        'C2:BSWV FRQ,{}'.format(fre),      # 频率配置为50 Hz
-        'C2:BSWV AMP,1.36',                # 幅度配置为1.36 V
-        'C2:BSWV OFST,680.E-3',            # 偏移量配置为780 mV
+        r'C1:BSWV FRQ,{}'.format(fre),      # 频率配置为50 Hz
+        r'C2:BSWV FRQ,{}'.format(fre),      # 频率配置为50 Hz
+        r'C2:BSWV AMP, {}'.format(amp_2),                # 幅度配置为1.36 V
+        r'C2:BSWV OFST, {}'.format(amp_2/2),            # 偏移量配置为780 mV
         r'C1:BSWV AMP,{}'.format(amp),     # 幅度配置为1.8V
         r'C1:BSWV OFST,{}'.format(amp/2.),  # 偏移量配置为900 mV (即0.9 V)
         'C1:BSWV WIDTH,150.E-9',  # S1脉冲宽度固定为150纳秒 (ns)
         'C2:BSWV WIDTH,1000.E-9',  # S2脉冲宽度固定为1us
         'C1:BSWV RISE, 1.E-9',   # 上升时间固定为1纳秒 (ns)
-        'C1:BSWV FALL, 1.E-9', # 下降时间固定为1纳秒 (ns)
         'C2:BSWV RISE, 1.E-9',   # 上升时间固定为1纳秒 (ns)
+        'C1:BSWV FALL, 1.E-9', # 下降时间固定为1纳秒 (ns)
         'C2:BSWV FALL, 1.E-9', # 下降时间固定为1纳秒 (ns)
         'C1:BSWV DLY, 0',      # CH1没有延迟, 单位为微秒 (us)
-        'C2:BSWV DLY, {}.E-6'.format(delay),      # CH2延迟时间配置, 单位为微秒 (us)
+        r'C2:BSWV DLY, {}.E-6'.format(delay),      # CH2延迟时间配置, 单位为微秒 (us)
     ]
     
     # 执行配置命令
@@ -74,7 +76,10 @@ def pulse_generator_configure():
         'C1:CoMBiNe?',
         'C2:CoMBiNe?',
         'C1:OUTP?',
-        'C2:OUTP?' ,       
+        'C2:OUTP?' ,   
+        'C1:BSWV?',    
+        'C2:BSWV?'
+
     ]
     print(rm_address, type(rm_address))    
     for info in config_info:
--- configuration.py
+++ configuration.py
@@ -1,15 +1,25 @@
 import config_pulse_gen as config
 import sys
+# import os
 
 argvs = sys.argv
-if len(argvs) < 4:
-    print('Usage: python configuration.py  fre=50 amp=1.8 offset=0.9 delay=5')
+
+if len(argvs) != 6:
+    print('Usage: python configuration.py  fre=50 amp=1.8 delay=5 sync=CH2 comb=True ' )
     sys.exit()
+
 fre =  int(sys.argv[1] )
 amp =  float(sys.argv[2] )
 delay =  int(sys.argv[3] )
+sync =  str(sys.argv[4] )
+comb =  sys.argv[5] 
 
-config.configure_waveform_generator(fre, amp, delay)
+if comb == 'True':
+    config.configure_waveform_generator(fre, amp, delay, sync, True)
+elif comb == 'False':
+    config.configure_waveform_generator(fre, amp, delay, sync, False)
+
+
 
 
 config.pulse_generator_configure()
